deb18bed527a24e875a5fc9f994d9611f90ebd9b
joaofscampos <59841168+joaofscampos@users.noreply.github.com>
2020-01-14 15:26:58 -0300

    corrigid 1

--- fase01/ex01/nomeDeVolta.js
+++ fase01/ex01/nomeDeVolta.js
@@ -1,5 +1,4 @@
-function nomeDeVolta() {
+function nomeDeVolta(){
     console.log("Marvin")
-    var marvin = ("o andróide paranóide")
-    return console.log("o andróide paranóide")
+    return console.log("O andróide paranóide")
 }
