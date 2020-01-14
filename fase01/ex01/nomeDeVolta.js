1d5d18f5ad343ec2864e32ed2d834b8223d5207d
joaofscampos <59841168+joaofscampos@users.noreply.github.com>
2020-01-14 16:00:50 -0300

    k

--- fase01/ex01/nomeDeVolta.js
+++ fase01/ex01/nomeDeVolta.js
@@ -1,4 +1,4 @@
-function nomeDeVolta(){
-    console.log("Marvin")
-    return console.log("O andróide paranóide")
+function nomeDeVolta() {
+    console.log ("Marvin")
+    return console.log "O andróide paranóide" 
 }
