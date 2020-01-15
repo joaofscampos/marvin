1ab6ab5baef0762761f7b0c4bdc2be2f9f0b4440
joaofscampos <59841168+joaofscampos@users.noreply.github.com>
2020-01-15 13:51:24 -0300

    ex 2 corrigido

--- fase01/ex02/nomes.js
+++ fase01/ex02/nomes.js
@@ -1,4 +1,4 @@
-function nomes (nome1  , nome2){
-    var nome3 =  nome1 +  " e "  + nome2;
-    return  nome3 
+function nomes(nome1, nome2){
+    var nome3 = nome1 + " e " + nome2;
+    return nome3
 }
