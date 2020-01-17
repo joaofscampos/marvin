0d07319d332d184ee7d753cf0f8c0ecd5a4d7ba3
joaofscampos <59841168+joaofscampos@users.noreply.github.com>
2020-01-17 15:38:42 -0300

    ex 4 certocerto

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -1,15 +1,15 @@
 function tempo(data1, data2, unidade){
-    var a = new Date (data1);
-    var c = new Date (data2);
-    var b = unidade;
-  var f = Math.abs(a.getTime() - c.getTime()); 
-  var g = Math.ceil(f / (1000 * 60 * 60 * 24));
-  var h = g/30;
-  var i = (g/30)/12;
-  if (b === "dia")
-  return g.toFixed(0);
-     else if (b=== "mes")
-     return h.toFixed(0);
-     else if (b === "ano")
-     return i.toFixed(0);
- }
+   var a = new Date (data1);
+   var b = new Date (data2);
+   var c = unidade;
+ var e = Math.abs(a.getTime() - b.getTime()); 
+ var f = Math.ceil(e / (1000 * 60 * 60 * 24));
+ var g = f/30;
+ var h = (f/30)/12;
+ if (c === "dia")
+ return Number(f.toFixed(0));
+    else if (c === "mês")
+    return Number(g.toFixed(0));
+    else if (c === "ano")
+    return Number(h.toFixed(0));
+}
