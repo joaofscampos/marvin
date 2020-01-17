88f9766ab3febd0fae61ef12c6aff3d262ada9aa
joaofscampos <59841168+joaofscampos@users.noreply.github.com>
2020-01-17 14:36:47 -0300

    ex 1 corrigido

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -1,10 +1,5 @@
-function nove(numero){
-    var a = numero.toString()
-var b =a.substr(-1)=="9"
-if (b){
-    return "true"
-}
-else{
-    return "false"
-} 
+function nove(numero) {
+var a = numero.toString()
+var b = a.substr(-1) == "9"
+return b
 }
